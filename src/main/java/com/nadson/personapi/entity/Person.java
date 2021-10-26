40aecc2e28fd4a6f159764f070feade767e7d7d8
Nadson Santana <nadsonlima10ns@gmail.com>
2021-10-25 23:39:11 -0300

    Fix call for superclass methods
    	- Fixed call for super Equals and HashCode with @EqualsAndHashCode annotation

--- src/main/java/com/nadson/personapi/entity/Person.java
+++ src/main/java/com/nadson/personapi/entity/Person.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
